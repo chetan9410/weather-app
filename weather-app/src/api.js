569d808b3b486aea31ebfc3ea9754a03bf4f2e96
chetan9410 <chetan9410@gmail.com>
2024-01-02 13:13:43 +0530

    Adding all files after building the search Component

---
+++ weather-app/src/api.js
@@ -0,0 +1,17 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a010e65108msh7ed33a901da7d81p184d78jsn2a171aad540c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+// try {
+//     const response = await fetch(url, options);
+//     const result = await response.text();
+//     console.log(result);
+// } catch (error) {
+//     console.error(error);
+// }
+
